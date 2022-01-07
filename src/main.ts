1b9a164fad11194be29e8d94593e59286e5023cd
Rakesh D R <rakesh@rapidinnovation.dev>
2022-01-07 18:09:46 +0530

    Added swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { setupSwagger } from './setup-swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.use(helmet());
+  app.setGlobalPrefix('/api');
+  setupSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
