aca3a014671956c67d27008310f28d007795e0d8
Rakesh D R <rakesh@rapidinnovation.dev>
2022-01-10 19:40:16 +0530

    Added docker compose

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,24 @@ import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 import { setupSwagger } from './setup-swagger';
+import { ApiConfigService } from './shared/services/api-config.service';
+import { SharedModule } from './shared/shared.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.use(helmet());
   app.setGlobalPrefix('/api');
-  setupSwagger(app);
 
-  await app.listen(3000);
+  const configService = app.select(SharedModule).get(ApiConfigService);
+
+  if (['development', 'staging'].includes(configService.nodeEnv)) {
+    setupSwagger(app);
+  }
+
+  const port = configService.getNumber('PORT');
+  await app.listen(port);
+
+  console.info(`server running on port ${port}`);
 }
 bootstrap();
